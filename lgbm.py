df20f72f67a7d3fcaf9dea50092b7ee6ddbb9cfd
Joey14123142 <14123142d@connect.polyu.hk>
2017-10-15 23:00:56 +0800

    add a Kagle kernel

--- lgbm.py
+++ lgbm.py
@@ -1,26 +1,28 @@
 import numpy as np
 import pandas as pd
+from time import gmtime, strftime
+import gc
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import (train_test_split, GridSearchCV)
 from sklearn.preprocessing import LabelEncoder
 import lightgbm as lgb
 from tqdm import tqdm
-from time import gmtime, strftime
 from sklearn.metrics import (roc_curve, auc, accuracy_score)
 
-from sklearn.model_selection import GridSearchCV
+### 1. Load data
 
+# read the first 99 rows for demo
+train = pd.read_csv('../train.csv', nrows=99)
+test = pd.read_csv('../test.csv',nrows=99)
+songs = pd.read_csv('../songs.csv')
+members = pd.read_csv('../members.csv')
 
-train = pd.read_csv('train.csv')#, nrows=999)
-test = pd.read_csv('test.csv')#,nrows=999)
-songs = pd.read_csv('songs.csv')#,nrows=999)
-members = pd.read_csv('members.csv')#,nrows=999)
-
-print('Data preprocessing...')
+# Merge datasets with song attributes
 song_cols = ['song_id', 'artist_name', 'genre_ids', 'song_length', 'language']
 train = train.merge(songs[song_cols], on='song_id', how='left')
 test = test.merge(songs[song_cols], on='song_id', how='left')
 
+# Merge datasets with member features
 members['registration_year'] = members['registration_init_time'].apply(lambda x: int(str(x)[0:4]))
 members['registration_month'] = members['registration_init_time'].apply(lambda x: int(str(x)[4:6]))
 members['registration_date'] = members['registration_init_time'].apply(lambda x: int(str(x)[6:8]))
@@ -37,7 +39,6 @@ test = test.merge(members[members_cols], on='msno', how='left')
 train = train.fillna(-1)
 test = test.fillna(-1)
 
-import gc
 del members, songs; gc.collect();
 
 cols = list(train.columns)
@@ -55,44 +56,35 @@ for col in tqdm(cols):
         train[col] = le.transform(train[col])
         test[col] = le.transform(test[col])
 
+
 X = np.array(train.drop(['target'], axis=1))
 y = train['target'].values
 
-params = {'boosting_type': 'gbdt',
-          'max_depth' : -1,
-          'objective': 'binary', 
-          'num_leaves': 64, 
-          'learning_rate': 0.05, 
-          'max_bin': 512, 
-          'subsample_for_bin': 200,
-          'subsample': 1, 
-          'subsample_freq': 1, 
-          'colsample_bytree': 0.8, 
-          'reg_alpha': 5, 
-          'reg_lambda': 10,
-          'min_split_gain': 0.5, 
-          'min_child_weight': 1, 
-          'min_child_samples': 5, 
-          'scale_pos_weight': 1,
-          'num_class' : 1,
-          'metric' : 'binary_error'}
 
-# Create parameters to search
-gridParams = {
-    'learning_rate': [0.005],
-    'n_estimators': [8,16,24],
-    'num_leaves': [6,8,12,16],
-    'boosting_type' : ['gbdt'],
-    'objective' : ['binary'],
-    'random_state' : [500],
-    'colsample_bytree' : [0.64, 0.65, 0.66],
-    'subsample' : [0.7,0.75],
-    'reg_alpha' : [1,1.2],
-    'reg_lambda' : [1,1.2,1.4],
-    }
+### 2. Initiate a model
 
-# Create classifier to use. Note that parameters have to be input manually
-# not as a dict!
+params = {
+    'application': 'binary', # for binary classification
+#     'num_class' : 1, # used for multi-classes
+    'boosting': 'gbdt', # traditional gradient boosting decision tree
+    'num_iterations': 100, 
+    'learning_rate': 0.05,
+    'num_leaves': 62,
+    'device': 'cpu', # you can use GPU to achieve faster learning
+    'max_depth': -1, # <0 means no limit
+    'max_bin': 510, # Small number of bins may reduce training accuracy but can deal with over-fitting
+    'lambda_l1': 5, # L1 regularization
+    'lambda_l2': 10, # L2 regularization
+    'metric' : 'binary_error',
+    'subsample_for_bin': 200, # number of samples for constructing bins
+    'subsample': 1, # subsample ratio of the training instance
+    'colsample_bytree': 0.8, # subsample ratio of columns when constructing the tree
+    'min_split_gain': 0.5, # minimum loss reduction required to make further partition on a leaf node of the tree
+    'min_child_weight': 1, # minimum sum of instance weight (hessian) needed in a leaf
+    'min_child_samples': 5# minimum number of data needed in a leaf
+}
+
+# Initiate classifier to use
 mdl = lgb.LGBMClassifier(boosting_type= 'gbdt', 
           objective = 'binary', 
           n_jobs = 5, 
@@ -101,16 +93,30 @@ mdl = lgb.LGBMClassifier(boosting_type= 'gbdt',
           max_bin = params['max_bin'], 
           subsample_for_bin = params['subsample_for_bin'],
           subsample = params['subsample'], 
-          subsample_freq = params['subsample_freq'], 
           min_split_gain = params['min_split_gain'], 
           min_child_weight = params['min_child_weight'], 
-          min_child_samples = params['min_child_samples'], 
-          scale_pos_weight = params['scale_pos_weight'])
+          min_child_samples = params['min_child_samples'])
 
-# To view the default model param[s:
+# To view the default model parameters:
 mdl.get_params().keys()
 
-# Create the grid
+
+### 3. Grid search
+
+gridParams = {
+    'learning_rate': [0.005, 0.01],
+    'n_estimators': [8,16,24],
+    'num_leaves': [6,8,12,16], # large num_leaves helps improve accuracy but might lead to over-fitting
+    'boosting_type' : ['gbdt', 'dart'], # for better accuracy -> try dart
+    'objective' : ['binary'],
+    'max_bin':[255, 510], # large max_bin helps improve accuracy but might slow down training progress
+    'random_state' : [500],
+    'colsample_bytree' : [0.64, 0.65, 0.66],
+    'subsample' : [0.7,0.75],
+    'reg_alpha' : [1,1.2],
+    'reg_lambda' : [1,1.2,1.4],
+    }
+
 grid = GridSearchCV(mdl, gridParams, verbose=1, cv=4, n_jobs=-1)
 # Run the grid
 grid.fit(X, y)
@@ -119,10 +125,11 @@ grid.fit(X, y)
 print(grid.best_params_)
 print(grid.best_score_)
 
-# Using parameters already set above, replace in the best from the grid search
+### 4. Use the best model
+
 params['colsample_bytree'] = grid.best_params_['colsample_bytree']
 params['learning_rate'] = grid.best_params_['learning_rate'] 
-# params['max_bin'] = grid.best_params_['max_bin']
+params['max_bin'] = grid.best_params_['max_bin']
 params['num_leaves'] = grid.best_params_['num_leaves']
 params['reg_alpha'] = grid.best_params_['reg_alpha']
 params['reg_lambda'] = grid.best_params_['reg_lambda']
@@ -145,7 +152,6 @@ watchlist = [d_train, d_valid]
 
 model = lgb.train(params, train_set=d_train, num_boost_round=1000, valid_sets=watchlist, early_stopping_rounds=50, verbose_eval=4)
 
-print('Making predictions and saving them...')
 p_test = model.predict(X_test)
 
 subm = pd.DataFrame()
@@ -153,5 +159,6 @@ subm['id'] = ids
 subm['target'] = p_test
 submName = strftime("%Y%m%d%H%M%S", gmtime()) + '_submission.csv.gz'
 subm.to_csv(submName, compression = 'gzip', index=False, float_format = '%.5f')
-print('Done!')
+
+
 
